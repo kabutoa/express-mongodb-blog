7a23e330d494489b227ac95ae0f7d216519edc73
kabutoa <yinyun957@163.com>
2022-02-15 18:00:43 +0800

    add ejs

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
 const indexRouter = require('./src/routes/index')
 const usersRouter = require('./src/routes/users')
+
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 router.get('/:name', (req, res) => {
-  res.send(`hello ${req.params.name}`)
+  res.render('users', {
+    name: req.params.name
+  })
 })
 
 module.exports = router
